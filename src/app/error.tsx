e13359f6d632c3717d1abc5df5829fc05b8682c8
ZAHEED-MOLLA <zaheedhossain06@gmail.com>
2024-04-10 06:55:20 +0530

    End of Project

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function ErrorPage(){
+    return <div className="space-y-3 text-center">
+        <h1 className="text-2xl font-bold">
+            Error
+        </h1>
+        <p>
+            Sorry, something went wrong.Please try again later.
+        </p>
+    </div>
+}
